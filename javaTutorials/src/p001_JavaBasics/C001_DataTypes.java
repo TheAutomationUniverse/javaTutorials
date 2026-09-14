9e537b1b202fb757ced0e8cc05e722e3f3ff7061
TheAutomationUniverse <aniket.korgaonkar25@outlook.com>
2026-09-14 22:51:09 +0530

    Adding Datatype definition to C001_DataTypes class

--- javaTutorials/src/p001_JavaBasics/C001_DataTypes.java
+++ javaTutorials/src/p001_JavaBasics/C001_DataTypes.java
@@ -1,10 +1,9 @@
 package p001_JavaBasics;
 
 public class C001_DataTypes {
+//	Datatype: Specifies size & type of value that can be stored in an identifier.
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
 }
